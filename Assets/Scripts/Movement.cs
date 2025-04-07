4dd8b6bfe23a767d9932d01b18c1f2d287337638
bilalaltinn <bilalaltn34@gmail.com>
2025-04-07 10:22:20 +0300

    ProjectBoost Version 1.2

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100;
+    [SerializeField] float rotationThrust = 1f;
+    Rigidbody rb;
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
